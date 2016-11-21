1f946a6ea647cf027583e4f0d0cd59ae9b80bc6c
Shubat Max <shubat.max@gmail.com>
2016-11-21 08:17:52 +0200

    Header and footer aka template

---
+++ app/views/footer_view.php
@@ -0,0 +1,9 @@
+            <!-- FOOTER - BEGIN -->
+            <div class="row">
+                FOOTER
+            </div>
+            <!-- FOOTER - END -->
+        </div>
+        <!-- WRAPPER - END -->
+    </body>
+</html>
---
+++ app/views/header_view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BeeJee Test App</title>
+</head>
+<body>
+<!-- WRAPPER -->
+<div class="container">
+
+    <!-- HEADER - BEGIN -->
+    <div class='row'>
+        <div class='col-lg-6'>
+            <a href="/"><h1 class='text-uppercase'>CommoNWealtH</h1></a>
+        </div>
+        <div class='col-lg-offset-4 col-lg-2'>
+            <a href='\Login' id='login' class="btn btn-primary btn-xs btn-block"><h4>Login</h4></a>
+        </div>
+    </div>
+    <!-- HEADER - END -->
