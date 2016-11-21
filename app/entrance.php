f6b99ad3237a5943c0273924c191de6d3c13ae98
Shubat Max <shubat.max@gmail.com>
2016-11-21 07:56:19 +0200

    Main entry point

---
+++ app/entrance.php
@@ -0,0 +1,17 @@
+<?php
+
+    //Core files
+    require_once 'core/controller.php';
+    require_once 'core/model.php';
+    require_once 'core/view.php';
+    require_once 'core/route.php';
+
+    //Side files
+    //require_once 'core/database';
+    //require_once 'core/sessions';
+
+    //Configs
+    require_once 'config/database.php';
+    require_once 'config/paths.php';
+
+    $app = new Route();
